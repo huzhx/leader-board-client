52001a23f0c805070bb9446e9a37f379d2ae86f2
Zhaoxian Hu <huzhx@uci.edu>
2021-05-31 19:52:43 -0700

    add overflow-auto

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Leaderboard from './components/Leaderboard';
 
 function App() {
   return (
-    <div className="w-screen h-screen p-8  bg-gray-100">
+    <div className="w-screen h-screen p-8  bg-gray-100 overflow-auto">
       <Leaderboard className="text-sm md:text-base lg:text-lg max-w-sm md:max-w-lg lg:max-w-3xl min-w-max m-auto flex-col justify-center items-center bg-white rounded-lg p-4 shadow-md divide-y-2 divide-gray-300 divide-opacity-20" />
     </div>
   );
